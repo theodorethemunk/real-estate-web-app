a89b52440b1a7128dc659d7a5feecff81a1a0cd8
Theodorethemunk <Sannioladimeji2003@gmail.com>
2026-03-15 12:29:20 +0100

    update API URL to Railway

--- real-estate-wep-app/src/repo/datarepo.tsx
+++ real-estate-wep-app/src/repo/datarepo.tsx
@@ -1,14 +1,4 @@
-//export const API_BASE_URL = "https://localhost:7271/api";
-export const API_BASE_URL = "https://sommyprop-001-site2.otempurl.com/api";
-
-//export const UPLOAD_URL = "https://localhost:7271";
-export const UPLOAD_URL = "https://sommyprop-001-site2.otempurl.com";
-
-//export const API_URL = "https://localhost:7271";
-export const API_URL = "https://sommyprop-001-site2.otempurl.com/";
-
-//export const API_ACCESS_TOKEN = "3766123bd101e4b636630c065173uenoLcb02666dc1233e5529efe0e6cf2062983123";
-
-//export const COMPANY_PRIMARY_DOMAIN_URL = "https://propertiessommy.com";
-export const COMPANY_PRIMARY_DOMAIN_URL = "https://sommyprop-001-site2.otempurl.com/";
-//export const COMPANY_PRIMARY_DOMAIN_URL = "https://localhost:7271/";
+export const API_BASE_URL = "https://real-estate-api-production-ac3a.up.railway.app/api";
+export const UPLOAD_URL = "https://real-estate-api-production-ac3a.up.railway.app";
+export const API_URL = "https://real-estate-api-production-ac3a.up.railway.app";
+export const COMPANY_PRIMARY_DOMAIN_URL = "https://real-estate-api-production-ac3a.up.railway.app/";
